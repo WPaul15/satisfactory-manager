8db4111c116a6f32b9b90a8f716482bd23225d75
wmpaul <willem.m.paul@gmail.com>
2020-12-21 14:55:10 -0800

    Initial commit

---
+++ src/main/java/com/wd40/satisfactorymanager/SatisfactorymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.wd40.satisfactorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatisfactorymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatisfactorymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wd40/satisfactorymanager/SatisfactorymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wd40.satisfactorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatisfactorymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
