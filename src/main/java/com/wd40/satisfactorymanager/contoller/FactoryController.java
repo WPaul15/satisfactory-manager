4d9c60a475981a5634b6735c31ea979cbc4c5d2e
Daniel <djddanman@gmail.com>
2020-12-21 22:55:41 -0600

    Add FactoryService

--- src/main/java/com/wd40/satisfactorymanager/contoller/FactoryController.java
+++ src/main/java/com/wd40/satisfactorymanager/contoller/FactoryController.java
@@ -1,19 +1,28 @@
 package com.wd40.satisfactorymanager.contoller;
 
 import com.wd40.satisfactorymanager.model.Factory;
+import com.wd40.satisfactorymanager.service.FactoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ManagerController {
+public class FactoryController {
+
+	private final FactoryService factoryService;
+
+	@Autowired
+	public FactoryController(FactoryService factoryService) {
+		this.factoryService = factoryService;
+	}
 
 	@PostMapping(
 		path = "/factory/{name}",
 		produces = MediaType.APPLICATION_JSON_VALUE
 	)
 	public Factory createNewFactory(@PathVariable("name") String name) {
-		return new Factory(name);
+		return factoryService.createNewFactory(name);
 	}
 }
