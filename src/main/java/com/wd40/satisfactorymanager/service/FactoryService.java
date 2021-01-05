06f4020f1fbeca83a284c2cf7782dc09c21da2ac
Daniel <djddanman@gmail.com>
2021-01-05 14:46:53 -0600

    Add add_machines branch Add addMachine Add generate_key

--- src/main/java/com/wd40/satisfactorymanager/service/FactoryService.java
+++ src/main/java/com/wd40/satisfactorymanager/service/FactoryService.java
@@ -22,4 +22,33 @@ public class FactoryService {
 	public Factory createNewFactory(String name) {
 		return factoryRepository.save(new Factory(name));
 	}
+
+	public void addMachine(String new_machine_type, String new_recipe, int new_clock, int count, String new_quality) {
+		String new_key = generate_key(new_machine_type, new_recipe, new_clock, new_quality);
+	}
+
+	public String generate_key(String machine_type, String recipe, int clock, String quality) {
+		// get machine key
+		// get recipe key
+
+		String machine_key = "";
+		String recipe_key = "";
+
+		String clock_key = String.format("%03d", clock);
+
+		String quality_key = "";
+		switch (quality) {
+			case "Impure":
+				quality_key = "i";
+				break;
+			case "Normal":
+				quality_key = "n";
+				break;
+			case "Pure":
+				quality_key = "p";
+				break;
+		}
+
+		return machine_key + quality_key + recipe_key + clock_key;
+	}
 }
