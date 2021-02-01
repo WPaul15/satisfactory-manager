33bf730423a7bca5d48b0d8c6889b3795f79828c
Willem Paul <willem.m.paul@gmail.com>
2021-01-31 18:22:07 -0600

    Turn off logging during tests

--- src/test/java/com/wd40/satisfactorymanager/controller/StaticDataControllerTest.java
+++ src/test/java/com/wd40/satisfactorymanager/controller/StaticDataControllerTest.java
@@ -16,8 +16,10 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = {StaticDataController.class})
+@ActiveProfiles("test")
 public class StaticDataControllerTest {
 
   private final StaticDataController staticDataController;
--- src/test/java/com/wd40/satisfactorymanager/service/StaticDataServiceTest.java
+++ src/test/java/com/wd40/satisfactorymanager/service/StaticDataServiceTest.java
@@ -8,8 +8,10 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = {StaticDataService.class})
+@ActiveProfiles("test")
 public class StaticDataServiceTest {
 
   private final StaticDataService staticDataService;
