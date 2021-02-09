f28c308037d2b086a9ecbd4b4e880b8d81ea5da5
Willem Paul <willem.m.paul@gmail.com>
2021-02-09 17:58:01 -0600

    Move update logic to service

--- src/main/java/com/wd40/satisfactorymanager/service/FactoryService.java
+++ src/main/java/com/wd40/satisfactorymanager/service/FactoryService.java
@@ -2,6 +2,7 @@ package com.wd40.satisfactorymanager.service;
 
 import com.wd40.satisfactorymanager.dto.request.change.MachineGroupChange;
 import com.wd40.satisfactorymanager.model.Factory;
+import com.wd40.satisfactorymanager.model.MachineGroup;
 import com.wd40.satisfactorymanager.repository.FactoryRepository;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,9 +38,26 @@ public class FactoryService {
     }
 
     if (changes != null && !changes.isEmpty()) {
-      factory.updateMachineGroups(changes);
+      updateMachineGroups(factory, changes);
     }
 
     return factoryRepository.save(factory);
+  }
+
+  private void updateMachineGroups(Factory factory, Map<String, MachineGroupChange> changes) {
+    Map<String, MachineGroup> machineGroups = factory.getMachineGroups();
+
+    changes.forEach(
+        (key, change) -> {
+          switch (change.getChangeOp()) {
+            case ADD:
+            case UPDATE:
+              machineGroups.put(key, change.getMachineGroup());
+              break;
+            case DELETE:
+              machineGroups.remove(key);
+              break;
+          }
+        });
   }
 }
